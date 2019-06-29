06d9d017162d9ff19698c9d47bc744cb74b62f99
sajicode <sajioloye@gmail.com>
2019-06-29 23:41:46 +0100

    modify user to sender

--- backend/src/models/Conversation.ts
+++ backend/src/models/Conversation.ts
@@ -1,34 +1,38 @@
 import { model, Schema, SchemaTypes, Document } from 'mongoose';
 
 interface ConversationDocument extends Document {
-  partyOne: string;
-  partyTwo: string;
-  messages: [{
-    user: string,
-    text: string,
-    created_at: Date
-  }],
-  created: Date;
+	partyOne: string;
+	partyTwo: string;
+	messages: [
+		{
+			sender: string;
+			text: string;
+			created_at: Date;
+		}
+	];
+	created: Date;
 }
 
 const ConversationSchema = new Schema({
-  partyOne: {
-    type: SchemaTypes.ObjectId,
-    required: true
-  },
-  partyTwo: {
-    type: SchemaTypes.ObjectId,
-    required: true
-  },
-  messages: [{
-    user: SchemaTypes.ObjectId,
-    text: SchemaTypes.String,
-    created_at: SchemaTypes.Date
-  }],
-  created: {
-    type: SchemaTypes.Date,
-    default: Date.now()
-  }
-})
+	partyOne: {
+		type: SchemaTypes.ObjectId,
+		required: true
+	},
+	partyTwo: {
+		type: SchemaTypes.ObjectId,
+		required: true
+	},
+	messages: [
+		{
+			sender: SchemaTypes.ObjectId,
+			text: SchemaTypes.String,
+			created_at: SchemaTypes.Date
+		}
+	],
+	created: {
+		type: SchemaTypes.Date,
+		default: Date.now()
+	}
+});
 
-export default model<ConversationDocument>('Conversation', ConversationSchema)
+export default model<ConversationDocument>('Conversation', ConversationSchema);
